ff07fece4fbdde10b04290634c8fc53a5829bdf1
langthanghy99@gmail.com <langthanghy99@gmail.com>
2022-03-17 11:02:45 +0700

    update

--- act.js
+++ act.js
@@ -1,4 +1,18 @@
 let keepN = document.querySelector('.form__card--keep');
+
+
+let keepdetail = 'Checking this box will reduce the number of times you’re asked to sign in. To keep your account secure, use this option only on your personal devices.';
+let forgotname = 'You can now use your email instead of a username.';
+
+
+
+
+
+let help = () => {
+   
+}
+
+
 let keepList = [...keepN.children];
 keepList.forEach( (keepbtnN, i) => {
    keepbtnN.onclick = () => {
